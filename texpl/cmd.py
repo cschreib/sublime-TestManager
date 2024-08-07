15e2db9dd7dbb20cec65b8f6c0a6f2a1155a66a1
Corentin Schreiber <github@corentin.net>
2024-08-07 14:30:43 +0100

    Add cmd_lines

--- texpl/cmd.py
+++ texpl/cmd.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import os
+import subprocess
 import sys
 import logging
 import threading
@@ -7,6 +9,7 @@ from functools import partial
 import sys
 import traceback
 import time
+from typing import Callable, List
 
 def get_thread_stack(thread):
     frame = sys._current_frames().get(thread.ident, None)
@@ -20,6 +23,13 @@ import sublime
 
 logger = logging.getLogger('TestExplorer.cmd')
 worker_logger = logging.getLogger('TestExplorerWorker')
+
+DECODING_ERROR = ("Could not decode output from {bin}. The following encodings "
+                  "were tried:\n\n"
+                  "{encodings}\n\n"
+                  "Try adjusting the fallback_encodings setting.")
+
+EXECUTABLE_ERROR = ("Executable '{bin}' was not found.")
 
 class JobError(Exception):
     pass
@@ -122,7 +132,7 @@ worker_thread = threading.Thread(target=process_queue)
 worker_thread.start()
 
 class Cmd:
-    def worker_run(self, job, task_id=None, timeout=None):
+    def worker_run(self, job: Callable, task_id=None, timeout=None):
         if not task_id:
             task_id = next_task_id()
 
@@ -149,7 +159,7 @@ class Cmd:
         worker_logger.info("[%s,%s] got output: %s", threading.get_ident(), task_id, str(outputs)[:32])
         return outputs
 
-    def worker_run_async(self, job, on_complete=None, on_exception=None, task_id=None, timeout=None):
+    def worker_run_async(self, job: Callable, on_complete=None, on_exception=None, task_id=None, timeout=None):
         if not task_id:
             task_id = next_task_id()
 
@@ -179,3 +189,72 @@ class Cmd:
                 logger.error('async-exception [BUG]: %s', e)
 
         return threading.Thread(target=partial(async_inner, job, on_complete, on_exception, task_id, timeout))
+
+    def cmd(self, command: List[str], stdin=None, cwd=None, env={}, ignore_errors=False, encoding='utf-8', fallback_encoding=[]):
+        environment = os.environ.copy()
+        environment.update(env)
+        task_id = next_task_id()
+
+        logger.debug("[%s,%s] cmd: %s", threading.get_ident(), task_id, command)
+
+        def job(command, stdin, cwd, environment, ignore_errors, encoding, fallback_encoding, task_id):
+            try:
+                if stdin and hasattr(stdin, 'encode'):
+                    stdin = stdin.encode(encoding)
+
+                proc = subprocess.Popen(command,
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT,
+                                        cwd=cwd,
+                                        env=environment)
+
+                stdout, stderr = proc.communicate(stdin)
+
+                logger.debug("[%s,%s] out: (%s) %s", threading.get_ident(), task_id, proc.returncode, [stdout[:100]])
+
+                return (proc.returncode, self.decode(stdout, encoding, fallback_encoding), self.decode(stderr, encoding, fallback_encoding))
+            except OSError as e:
+                if ignore_errors:
+                    return (0, '', '')
+                sublime.error_message(self.get_executable_error(command[0]))
+                return JobError("[%s,%s] Could not execute command: %s" % (threading.get_ident(), task_id, e))
+            except UnicodeDecodeError as e:
+                if ignore_errors:
+                    return (0, '', '')
+                sublime.error_message(self.get_decoding_error(command[0], encoding, fallback_encoding))
+                return JobError("[%s,%s] Could not execute command: %s" % (threading.get_ident(), task_id, command))
+
+        return self.worker_run(partial(job, command, stdin, cwd, environment, ignore_errors, encoding, fallback_encoding, task_id), task_id=task_id)
+
+    def cmd_lines(self, command: List[str], ignore_errors=False, success_codes=[0], *args, **kwargs):
+        error_code, stdout, stderr = self.cmd(command, *args, ignore_errors=ignore_errors, **kwargs)
+        if not ignore_errors and error_code not in success_codes:
+            message = stdout if stderr is None else stderr
+            raise JobError(f'Error when executing command {command}: {message}')
+
+        stdout = stdout.rstrip()
+        if not stdout:
+            return []
+        return stdout.split('\n')
+
+    def decode(self, stream, encoding, fallback_encoding=[]):
+        if not hasattr(stream, 'decode'):
+            return stream
+
+        try:
+            return stream.decode(encoding)
+        except UnicodeDecodeError:
+            for enc in fallback_encoding:
+                try:
+                    return stream.decode(enc)
+                except UnicodeDecodeError:
+                    pass
+            raise
+
+    def get_executable_error(self, bin):
+        return EXECUTABLE_ERROR.format(bin=bin)
+
+    def get_decoding_error(self, bin, encoding, fallback_encoding=[]):
+        encodings = [encoding] + fallback_encoding
+        return DECODING_ERROR.format(encodings="\n".join(encodings), bin=bin)
