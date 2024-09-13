43f804ab5fc3b434e58dc5ec2d3c454836e862a1
Corentin Schreiber <github@corentin.net>
2024-09-13 10:08:40 +0100

    Added cmd_streamed

--- texpl/cmd.py
+++ texpl/cmd.py
@@ -9,6 +9,7 @@ from functools import partial
 import sys
 import traceback
 import time
+import atexit
 from typing import Callable, List
 
 def get_thread_stack(thread):
@@ -34,198 +35,203 @@ EXECUTABLE_ERROR = ("Executable '{bin}' was not found.")
 class JobError(Exception):
     pass
 
-worker_queue = queue.Queue(1)
-output_queue = {}
-output_queue_lock = threading.Lock()
-last_task_id = 0
-max_tries = 100
+class WorkQueue:
+    max_tries = 100
 
-def get_output_queue(task_id):
-    global output_queue_lock
-    global output_queue
+    def __init__(self, name):
+        self.name = name
+        self.worker_queue = queue.Queue(1)
+        self.output_queue = {}
+        self.output_queue_lock = threading.Lock()
+        self.last_task_id = 0
 
-    with output_queue_lock:
-        if not task_id in output_queue:
-            output_queue[task_id] = queue.Queue(1)
+        self.worker_thread = threading.Thread(target=self.process_queue)
+        self.worker_thread.start()
 
-        return output_queue[task_id]
+    def get_output_queue(self, task_id):
+        with self.output_queue_lock:
+            if not task_id in self.output_queue:
+                self.output_queue[task_id] = queue.Queue(1)
 
-def release_output_queue(task_id):
-    global output_queue_lock
-    global output_queue
+            return self.output_queue[task_id]
 
-    with output_queue_lock:
-        if task_id in output_queue:
-            del output_queue[task_id]
+    def release_output_queue(self, task_id):
+        with self.output_queue_lock:
+            if task_id in self.output_queue:
+                del self.output_queue[task_id]
 
-def process_queue_one():
-    task_id, job = worker_queue.get(timeout=1)
-    worker_logger.info("[%s,%s] got input, processing...", threading.get_ident(), task_id)
+    def process_queue_one(self):
+        task_id, job = self.worker_queue.get(timeout=1)
+        worker_logger.info("[%s,%s,%s] got input, processing...", self.name, threading.get_ident(), task_id)
 
-    try:
-        outputs = job()
-        worker_logger.info("[%s,%s] got output, sending...", threading.get_ident(), task_id)
-    except Exception as e:
-        worker_logger.warning("[%s,%s] got error: %s\n%s", threading.get_ident(), task_id, e, traceback.format_exc())
-        worker_logger.warning("[%s,%s] sending...", threading.get_ident(), task_id)
-        outputs = JobError("Unhandled exception in queue command: %s" % e)
-
-    get_output_queue(task_id).put(outputs, timeout=1)
-
-    worker_logger.info("[%s,%s] sent", threading.get_ident(), task_id)
-
-def process_queue():
-    while True:
         try:
-            worker_logger.debug("wait for input...")
-            process_queue_one()
-        except:
-            worker_logger.debug("no input")
-            pass
-
-def next_task_id():
-    global last_task_id
-    last_task_id += 1
-    task_id = last_task_id
-    return task_id
-
-def dump_stack(operation, task_id):
-    worker_logger.warning("[%s,%s] %s timed out, worker thread stack:", threading.get_ident(), task_id, operation)
-    for entry in get_thread_stack(worker_thread):
-        worker_logger.warning("[%s,%s] " + entry.replace('\r', '').split('\n')[0])
-
-def push_new_job(task_id, job):
-    num_tries = 0
-    while True:
-        try:
-            worker_queue.put((task_id, job), timeout=0.1)
-            return
+            outputs = job()
+            worker_logger.info("[%s,%s,%s] got output, sending...", self.name, threading.get_ident(), task_id)
         except Exception as e:
-            worker_logger.debug("[%s,%s] put timed out, waiting some more...", threading.get_ident(), task_id)
-            num_tries += 1
-            if num_tries == max_tries:
-                dump_stack('put', task_id)
-                raise e
+            worker_logger.warning("[%s,%s,%s] got error: %s\n%s", self.name, threading.get_ident(), task_id, e, traceback.format_exc())
+            worker_logger.warning("[%s,%s,%s] sending...", self.name, threading.get_ident(), task_id)
+            outputs = JobError("Unhandled exception in queue command: %s" % e)
 
-def get_job_output(task_id, timeout=None):
-    try:
-        num_tries = 0
-        queue = get_output_queue(task_id)
+        self.get_output_queue(task_id).put(outputs, timeout=1)
 
-        start = time.time()
-        while timeout is None or time.time() - start < timeout:
+        worker_logger.info("[%s,%s,%s] sent", threading.get_ident(), task_id)
+
+    def process_queue(self):
+        while True:
             try:
-                return queue.get(timeout=0.1)
+                worker_logger.debug("wait for input...")
+                self.process_queue_one()
+            except:
+                worker_logger.debug("no input")
+                pass
+
+    def next_task_id(self):
+        self.last_task_id += 1
+        task_id = self.last_task_id
+        return task_id
+
+    def dump_stack(self, operation, task_id):
+        worker_logger.warning("[%s,%s,%s] %s timed out, worker thread stack:", self.name, threading.get_ident(), task_id, operation)
+        for entry in get_thread_stack(self.worker_thread):
+            worker_logger.warning("[%s,%s,%s] " + entry.replace('\r', '').split('\n')[0])
+
+    def push_new_job(self, task_id, job):
+        num_tries = 0
+        while True:
+            try:
+                self.worker_queue.put((task_id, job), timeout=0.1)
+                return
             except Exception as e:
-                worker_logger.debug("[%s,%s] get timed out, waiting some more...", threading.get_ident(), task_id)
+                worker_logger.debug("[%s,%s,%s] put timed out, waiting some more...", self.name, threading.get_ident(), task_id)
                 num_tries += 1
-                if num_tries == max_tries:
-                    dump_stack('get', task_id)
+                if num_tries == self.max_tries:
+                    self.dump_stack('put', task_id)
                     raise e
 
-        raise Exception(f'Job timeout for task {task_id}')
+    def get_job_output(self, task_id, timeout=None):
+        try:
+            num_tries = 0
+            queue = self.get_output_queue(task_id)
 
-    finally:
-        release_output_queue(task_id)
+            start = time.time()
+            while timeout is None or time.time() - start < timeout:
+                try:
+                    return queue.get(timeout=0.1)
+                except Exception as e:
+                    worker_logger.debug("[%s,%s,%s] get timed out, waiting some more...", self.name, threading.get_ident(), task_id)
+                    num_tries += 1
+                    if num_tries == self.max_tries:
+                        self.dump_stack('get', task_id)
+                        raise e
 
-worker_thread = threading.Thread(target=process_queue)
-worker_thread.start()
+            raise Exception(f'Job timeout for task {task_id}')
+
+        finally:
+            self.release_output_queue(task_id)
+
+
+work_queues = {}
+queue_list_lock = threading.Lock()
+
+def get_queue(name: str) -> WorkQueue:
+    global work_queues
+    global queue_list_lock
+
+    with queue_list_lock:
+        if not name in work_queues:
+            work_queues[name] = WorkQueue(name)
+
+        return work_queues[name]
+
+
+def decode(stream, encoding, fallback_encoding=[]):
+    if not hasattr(stream, 'decode'):
+        return stream
+
+    try:
+        return stream.decode(encoding)
+    except UnicodeDecodeError:
+        for enc in fallback_encoding:
+            try:
+                return stream.decode(enc)
+            except UnicodeDecodeError:
+                pass
+        raise
+
 
 class Cmd:
-    def worker_run(self, job: Callable, task_id=None, timeout=None):
+    def worker_run(self, job: Callable, queue : WorkQueue, task_id=None, timeout=None):
         if not task_id:
-            task_id = next_task_id()
+            task_id = queue.next_task_id()
 
-        worker_logger.info("[%s,%s] running task", threading.get_ident(), task_id)
+        worker_logger.info("[%s,%s,%s] running task", queue.name, threading.get_ident(), task_id)
 
-        if threading.get_ident() == worker_thread.ident:
+        if threading.get_ident() == queue.worker_thread.ident:
             # We are already in the worker thread, execute immediately
-            worker_logger.info("[%s,%s] immediate call", threading.get_ident(), task_id)
+            worker_logger.info("[%s,%s,%s] immediate call", queue.name, threading.get_ident(), task_id)
             outputs = job()
         else:
             # Schedule on the worker thread and wait for the output now
             try:
-                worker_logger.info("[%s,%s] new input, sending...", threading.get_ident(), task_id)
-                push_new_job(task_id, job)
-                worker_logger.info("[%s,%s] wait for output...", threading.get_ident(), task_id)
-                outputs = get_job_output(task_id, timeout=timeout)
+                worker_logger.info("[%s,%s,%s] new input, sending...", queue.name, threading.get_ident(), task_id)
+                queue.push_new_job(task_id, job)
+                worker_logger.info("[%s,%s,%s] wait for output...", queue.name, threading.get_ident(), task_id)
+                outputs = queue.get_job_output(task_id, timeout=timeout)
             except Exception as e:
                 outputs = JobError("Could not execute command: %s" % e)
 
         if isinstance(outputs, Exception):
-            worker_logger.info("[%s,%s] got error", threading.get_ident(), task_id)
+            worker_logger.info("[%s,%s,%s] got error", queue.name, threading.get_ident(), task_id)
             raise outputs
 
-        worker_logger.info("[%s,%s] got output: %s", threading.get_ident(), task_id, str(outputs)[:32])
+        worker_logger.info("[%s,%s,%s] got output: %s", queue.name, threading.get_ident(), task_id, str(outputs)[:32])
         return outputs
 
-    def worker_run_async(self, job: Callable, on_complete=None, on_exception=None, task_id=None, timeout=None):
-        if not task_id:
-            task_id = next_task_id()
+    def cmd(self, command: List[str], queue='default', stdin=None, cwd=None, env={}, stream_reader=None, ignore_errors=False, encoding='utf-8', fallback_encoding=[]):
+        queue = get_queue(queue)
 
-        worker_logger.info("[%s,%s] running async task", threading.get_ident(), task_id)
-
-        # Spawn a thread to schedule the job on the worker thread and wait for the output
-        def async_inner(job, on_complete, on_exception, task_id, timeout):
-            try:
-                try:
-                    worker_logger.info("[%s,%s] async new input, sending...", threading.get_ident(), task_id)
-                    push_new_job(task_id, job)
-                    worker_logger.info("[%s,%s] async wait for output...", threading.get_ident(), task_id)
-                    outputs = get_job_output(task_id, timeout=timeout)
-                except Exception as e:
-                    outputs = JobError("Could not execute command: %s" % e)
-
-                if isinstance(outputs, Exception):
-                    worker_logger.info("[%s,%s] async got error", threading.get_ident(), task_id)
-                    logger.debug('async-exception: %s', outputs)
-                    if callable(on_exception):
-                        sublime.set_timeout(partial(on_exception, outputs), 0)
-                else:
-                    worker_logger.info("[%s,%s] async got output", threading.get_ident(), task_id)
-                    if callable(on_complete):
-                        sublime.set_timeout(partial(on_complete, outputs), 0)
-            except Exception as e:
-                logger.error('async-exception [BUG]: %s', e)
-
-        return threading.Thread(target=partial(async_inner, job, on_complete, on_exception, task_id, timeout))
-
-    def cmd(self, command: List[str], stdin=None, cwd=None, env={}, ignore_errors=False, encoding='utf-8', fallback_encoding=[]):
         environment = os.environ.copy()
         environment.update(env)
-        task_id = next_task_id()
+        task_id = queue.next_task_id()
 
         logger.debug("[%s,%s] cmd: %s", threading.get_ident(), task_id, command)
 
-        def job(command, stdin, cwd, environment, ignore_errors, encoding, fallback_encoding, task_id):
+        def job(command, queue, stdin, cwd, environment, stream_reader, ignore_errors, encoding, fallback_encoding, task_id):
             try:
                 if stdin and hasattr(stdin, 'encode'):
                     stdin = stdin.encode(encoding)
 
-                proc = subprocess.Popen(command,
+                with subprocess.Popen(command,
                                         stdin=subprocess.PIPE,
                                         stdout=subprocess.PIPE,
                                         stderr=subprocess.STDOUT,
                                         cwd=cwd,
-                                        env=environment)
+                                        env=environment) as proc:
+                    if stream_reader is not None:
+                        for line in proc.stdout:
+                            stream_reader(decode(line, encoding, fallback_encoding))
 
-                stdout, stderr = proc.communicate(stdin)
+                        return (proc.poll(), None, None)
+                    else:
+                        stdout, stderr = proc.communicate(stdin)
+                        stdout = decode(stdout, encoding, fallback_encoding)
+                        stderr = decode(stderr, encoding, fallback_encoding)
 
-                logger.debug("[%s,%s] out: (%s) %s", threading.get_ident(), task_id, proc.returncode, [stdout[:100]])
+                        logger.debug("[%s,%s,%s] out: (%s) %s", queue.name, threading.get_ident(), task_id, proc.returncode, [stdout[:100]])
 
-                return (proc.returncode, self.decode(stdout, encoding, fallback_encoding), self.decode(stderr, encoding, fallback_encoding))
+                        return (proc.returncode, stdout, stderr)
             except OSError as e:
                 if ignore_errors:
                     return (0, '', '')
                 sublime.error_message(self.get_executable_error(command[0]))
-                return JobError("[%s,%s] Could not execute command: %s" % (threading.get_ident(), task_id, e))
+                return JobError("[%s,%s,%s] Could not execute command: %s" % (threading.get_ident(), task_id, e))
             except UnicodeDecodeError as e:
                 if ignore_errors:
                     return (0, '', '')
                 sublime.error_message(self.get_decoding_error(command[0], encoding, fallback_encoding))
-                return JobError("[%s,%s] Could not execute command: %s" % (threading.get_ident(), task_id, command))
+                return JobError("[%s,%s,%s] Could not execute command: %s" % (threading.get_ident(), task_id, command))
 
-        return self.worker_run(partial(job, command, stdin, cwd, environment, ignore_errors, encoding, fallback_encoding, task_id), task_id=task_id)
+        return self.worker_run(partial(job, command, queue, stdin, cwd, environment, stream_reader, ignore_errors, encoding, fallback_encoding, task_id), queue, task_id=task_id)
 
     def cmd_string(self, command: List[str], ignore_errors=False, success_codes=[0], *args, **kwargs):
         error_code, stdout, stderr = self.cmd(command, *args, ignore_errors=ignore_errors, **kwargs)
@@ -243,19 +249,11 @@ class Cmd:
         stdout = self.cmd_string(command, ignore_errors=ignore_errors, success_codes=success_codes, *args, **kwargs)
         return stdout.split('\n')
 
-    def decode(self, stream, encoding, fallback_encoding=[]):
-        if not hasattr(stream, 'decode'):
-            return stream
-
-        try:
-            return stream.decode(encoding)
-        except UnicodeDecodeError:
-            for enc in fallback_encoding:
-                try:
-                    return stream.decode(enc)
-                except UnicodeDecodeError:
-                    pass
-            raise
+    def cmd_streamed(self, command: List[str], stream_reader, ignore_errors=False, success_codes=[0], *args, **kwargs):
+        error_code, _, _ = self.cmd(command, *args, stream_reader=stream_reader, ignore_errors=ignore_errors, **kwargs)
+        if not ignore_errors and error_code not in success_codes:
+            command_str = ' '.join(command)
+            raise JobError(f'Error when executing command "{command_str}" (exit code {error_code}).')
 
     def get_executable_error(self, bin):
         return EXECUTABLE_ERROR.format(bin=bin)
