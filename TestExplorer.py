e6b640748e471f4d9c3f1b92f60b57d0cf5425d5
Corentin Schreiber <github@corentin.net>
2024-09-13 10:02:23 +0100

    Better logging and reload mechanisms

--- TestExplorer.py
+++ TestExplorer.py
@@ -1,46 +1,16 @@
 # coding: utf-8
-import sys
 import logging
 
-import sublime
+log_format = "[%(asctime)s - %(levelname)-8s - %(name)s] %(message)s"
+logging.basicConfig(level=logging.WARNING, format=log_format)
+logging.basicConfig(filename='~/.subl.log', encoding='utf-8', level=logging.DEBUG)
 
-# set up some logging
-logging.basicConfig(level=logging.WARNING, format="[%(asctime)s - %(levelname)-8s - %(name)s] %(message)s")
 logger = logging.getLogger('TestExplorer')
 worker_logger = logging.getLogger('TestExplorerWorker')
+parser_logger = logging.getLogger('TestExplorerParser')
 
-# reload modules if necessary
-LOAD_ORDER = [
-    # base
-    '',
-    '.util',
-    '.cmd',
-    '.test_data',
-    '.test_framework',
-    '.helpers',
-
-    # commands
-    '.list',
-    '.discover',
-    '.run',
-
-    # meta
-    '.TestExplorer',
-]
-
-needs_reload = [n for n, m in list(sys.modules.items()) if n[0:4] == 'texpl' and m is not None]
-
-reloaded = []
-for postfix in LOAD_ORDER:
-    module = 'texpl' + postfix
-    if module in needs_reload:
-        reloaded.append(module)
-        reload(sys.modules[module])
-if reloaded:
-    logger.info('Reloaded %s' % ", ".join(reloaded))
-
-# import commands and listeners
-from .texpl import *  # noqa
+import sublime
+from .texpl import *
 
 def plugin_loaded():
     settings = sublime.load_settings('TestExplorer.sublime-settings')
@@ -51,6 +21,17 @@ def plugin_loaded():
 
     worker_lvl = getattr(logging, settings.get('worker_log_level', '').upper(), logging.WARNING)
     worker_logger.setLevel(worker_lvl)
+
+    parser_lvl = getattr(logging, settings.get('parser_log_level', '').upper(), logging.WARNING)
+    parser_logger.setLevel(parser_lvl)
+
+    # set file output
+    filename = settings.get('log_file')
+    if filename:
+        handler = logging.FileHandler(filename)
+        formatter = logging.Formatter(fmt=log_format, style='%')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
 def plugin_unloaded():
     logging.shutdown()
